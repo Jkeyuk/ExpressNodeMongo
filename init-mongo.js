e8c923c6ea5e1d0cc16a5ad0747b663c41d3a98c
Jkeyuk <jonkero2@gmail.com>
2023-03-15 17:19:05 -0400

    Adjust docker to expose database with init script

---
+++ init-mongo.js
@@ -0,0 +1,22 @@
+let res = [
+  db.acronym.drop(),
+  db.acronym.createIndex({ acronym: 1 }),
+  db.acronym.createIndex({ definition: 1 }),
+  db.acronym.insertMany([
+    {
+      _id: "507f191e810c19729de860eb",
+      acronym: "2B",
+      definition: "To be",
+    },
+    {
+      _id: "507f191e810c19729de860ec",
+      acronym: "2EZ",
+      definition: "Too easy",
+    },
+    {
+      _id: "507f191e810c19729de860ed",
+      acronym: "2G2BT",
+      definition: "Too good to be true",
+    },
+  ]),
+];
