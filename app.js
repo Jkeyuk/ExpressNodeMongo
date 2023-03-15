8b580db5e5712f2fc791a077dc0a25be7b7749d6
Jkeyuk <jonkero2@gmail.com>
2023-03-15 18:13:47 -0400

    Adjust app.js to connect to the database

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+
+const client = new MongoClient("mongodb://root:example@127.0.0.1:27017/", { monitorCommands: true });
+client.connect();
+const db = client.db("acronym");
+
 const app = express();
 const port = 3000;
 
-app.get("/acronym?page=1&limit=10&search=:search", (req, res) => {
-  res.send("Hello World!");
+app.get("/acronym", async (req, res) => {
+  let results = await db.collection("acronym").find({}).limit(50).toArray();
+  res.send(results).status(200);
 });
 
 app.post("/acronym", (req, res) => {
@@ -18,6 +25,6 @@ app.delete("/acronym/:acronymID", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Example app listening on port ${port}`);
 });
