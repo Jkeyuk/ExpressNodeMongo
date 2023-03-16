44a6e224a0edeeefad9875a1cc47a92498e64c95
Jkeyuk <jonkero2@gmail.com>
2023-03-16 08:06:24 -0400

    add post fuctionality

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const bp = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 
-const client = new MongoClient("mongodb://root:example@127.0.0.1:27017/", { monitorCommands: true });
+const client = new MongoClient("mongodb://root:example@127.0.0.1:27017/", {
+  monitorCommands: true,
+});
 client.connect();
 const db = client.db("acronym");
 
 const app = express();
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+
 const port = 3000;
 
 app.get("/acronym", async (req, res) => {
@@ -13,8 +19,11 @@ app.get("/acronym", async (req, res) => {
   res.send(results).status(200);
 });
 
-app.post("/acronym", (req, res) => {
-  res.send("Hello World!");
+app.post("/acronym", async (req, res) => {
+  let collection = db.collection("acronym");
+  let newDocument = req.body;
+  let result = await collection.insertOne(newDocument);
+  res.send(result).status(204);
 });
 
 app.patch("/acronym/:acronymID", (req, res) => {
